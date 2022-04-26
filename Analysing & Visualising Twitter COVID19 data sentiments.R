1f282426231ea9f4257354bdf1ae3b4a79eb641e
maddyhyc <53378223+maddyhyc@users.noreply.github.com>
2022-04-26 23:35:11 +0930

    Update Analysing & Visualising Twitter COVID19 data sentiments.R

--- Analysing & Visualising Twitter COVID19 data sentiments.R
+++ Analysing & Visualising Twitter COVID19 data sentiments.R
@@ -21,21 +21,25 @@ scoredate <- colSums(sa.value_date[,]) #summing scores
 score_df_date <- data.frame(scoredate) 
 sa.scoredate <- cbind(sentiment = row.names(score_df_date), score_df_date, row.names=NULL)
 
-# In the video I showed the example with data from Mar 29 - Mar 31 which I took out here
+# If you start with your own tweets extract, combine all the daily data like this step
+# For the purpose of this exercise, this step is already done and all data is combined in score_df.csv
+# Show collected data from Mar 29 - Apr 11
+score_df <- cbind(score_df_mar29, score_df_mar30, score_df_mar31, score_df_apr1, score_df_apr2, score_df_apr3, score_df_apr4, score_df_apr5, score_df_apr6, score_df_apr7, score_df_apr8, score_df_apr9, score_df_apr10, score_df_apr11 ) 
+
+# In the video I showed the example with data from Mar 29 - Mar 31 which I left out here
 # I included here data collected from Mar 29 - Apr 11 which you can upload using read.csv()
 # Once uploaded, you should be able to follow the steps below
 score_df <- read.csv("score_df.csv")
 
-# Show collected data from Mar 29 - Apr 11
+# Drop first column
+score_df <- score_df[,-1]
 
-score_df <- cbind(score_df_mar29, score_df_mar30, score_df_mar31, score_df_apr1, score_df_apr2, score_df_apr3, score_df_apr4, score_df_apr5, score_df_apr6, score_df_apr7, score_df_apr8, score_df_apr9, score_df_apr10, score_df_apr11 )
-score_df <- cbind(sentiment = row.names(score_df), score_df, row.names=NULL)
+# Run this if you need to rename columns if not dont run. Do make sure that column names are in order
 colnames(score_df)<- c("sentiment", "mar29", "mar30", "mar31", "apr1", "apr2", "apr3", "apr4", "apr5", "apr6", "apr7", "apr8","apr9", "apr10", "apr11")
 
 # Using tidyr to reformat the table
 library(tidyr)
-score_df2 <- gather(score_df, 'mar29', 'mar30', 'mar31', 'apr1', 'apr2', 'apr3', 'apr4', 'apr5', 'apr6', 'apr7', 'apr8', 'apr9', 'apr10', 'apr11', key = "day", value = "score", na.rm = FALSE)
-
+score_df2 <- gather(score_df, 'mar29', 'mar30', 'mar31', 'apr1', 'apr2', 'apr3', 'apr4', 'apr5', 'apr6', 'apr7', 'apr8', 'apr9', 'apr10', 'apr11', key = "day", value = "score", na.rm = FALSE) #For an updated gather() function, use pivot_longer()
 # Use ggplot to plot graphs
 library(ggplot2)
 library(forcats)
